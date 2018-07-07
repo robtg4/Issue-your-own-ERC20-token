8561f5d8b820764f4d9e607b7f7b5f636754212e
Robby Greenfield <robertgreenv@gmail.com>
2018-07-07 01:34:32 -0700

    update erc20_tutorial.sol
    take all cuss words out for tutorial purposes

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'D0PE' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
+// Deployed to : YOUR WALLET PUBLIC ADDRESS
+// Symbol      : 0DOPE
+// Name        : Black Girls Code DopeToken
 // Total supply: 100000000
 // Decimals    : 18
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract DopeToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -112,13 +112,13 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    function FucksToken() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+    function DopeToken() public {
+        symbol = "0DOPE";
+        name = "Black Girls Code DopeToken";
         decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        _totalSupply = 100000000000000000000; //1000 tokens
+        balances[YOUR WALLET PUBLIC ADDRESS] = _totalSupply;
+        Transfer(address(0), YOUR WALLET PUBLIC ADDRESS, _totalSupply);
     }
 
 
